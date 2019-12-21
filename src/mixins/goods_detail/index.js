60607de8ed44c07a828f0e4164b247833edce626
supertinypeaunt <1176032017@qq.com>
2019-12-21 20:52:22 +0800

    商品详情页轮播图渲染与全屏显示

--- src/mixins/goods_detail/index.js
+++ src/mixins/goods_detail/index.js
@@ -3,16 +3,33 @@ import wepy from 'wepy'
 export default class extends wepy.mixin {
   data = {
     // 商品所有信息
-    message: {}
+    goods: {}
   }
   onLoad(options) {
     // 获取数据商品详情
     this.getDetail(options.goods_id)
   }
 
-  //
+  methods = {
+    // 图片全屏预览
+    perview(url) {
+      wepy.previewImage({
+        urls: this.goods.pics.map(item => item.pics_big_url),
+        current: url
+      })
+    }
+  }
+
+  // 请求商品详情数据
   async getDetail(id) {
     const { data } = await wepy.get('goods/detail', { goods_id: id })
-    this.message = data.message
+
+    // 数据获取失败
+    if (data.meta.status !== 200) {
+      return wepy.baseToast()
+    }
+
+    this.goods = data.message
+    this.$apply()
   }
 }
