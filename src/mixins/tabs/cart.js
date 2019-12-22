ad5becce0830df2249aa9c95cd44ae091e0fed84
supertinypeaunt <1176032017@qq.com>
2019-12-22 17:28:27 +0800

    购物车选中状态修改

--- src/mixins/tabs/cart.js
+++ src/mixins/tabs/cart.js
@@ -1,9 +1,20 @@
 import wepy from 'wepy'
 
 export default class extends wepy.mixin {
+  methods = {
+    //  更改选中状态
+    onChooseChange(e) {
+      console.log(e)
+      // 获取id和done
+      const id = e.target.dataset.id
+      const done = e.detail
+    // 触发全局修改选中状态事件
+      this.$parent.changeGoodsChoose(id, done)
+    }
+  }
   computed = {
+    // 购物车数据列表
     carts() {
-      console.log(this.$parent.globalData.carts)
       return this.$parent.globalData.carts
     }
   }
