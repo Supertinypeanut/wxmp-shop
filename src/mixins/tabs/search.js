5451b187be61220460708c0b225121fae73c9ffa
supertinypeaunt <1176032017@qq.com>
2019-12-23 12:09:45 +0800

    对搜索页下拉联想进行防抖处理与非空校验

--- src/mixins/tabs/search.js
+++ src/mixins/tabs/search.js
@@ -2,23 +2,36 @@ import wepy from 'wepy'
 
 export default class extends wepy.mixin {
   data = {
-    responseData: []
+    // 联想数据
+    responseData: [],
+    // 定时器器id
+    timer: undefined
   }
 
   methods = {
     // 监听搜索框改变
-    async onChange(e) {
-          // 搜索关键词
-      const query = e.detail
-          // 发送请求
-      const { data } = await wepy.get('goods/qsearch', {query})
-          // 判断是请求成功
-      if (data.meta.status !== 200) {
-        return wepy.baseToast()
-      }
-          // 更新响应数据
-      this.responseData = data.message
-      this.$apply()
+    onChange(e) {
+      // 清除上次定时器
+      clearTimeout(this.timer)
+      // 防抖处理
+      this.timer = setTimeout(async() => {
+      // 搜索关键词
+        const query = String.prototype.trim.call(e.detail)
+      // 对搜索词进行非空校验和请求是否完成校验
+        if (!query) {
+          return
+        }
+
+      // 发送请求
+        const { data } = await wepy.get('goods/qsearch', {query})
+        // 判断是请求成功
+        if (data.meta.status !== 200) {
+          return wepy.baseToast()
+        }
+      // 更新响应数据
+        this.responseData = data.message
+        this.$apply()
+      }, 500)
     },
 
     // 点击下拉联想跳转详情页
