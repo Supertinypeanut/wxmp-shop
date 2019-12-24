edf6b60d7ecdfd5686a3a8642b3f381e64003df1
supertinypeaunt <1176032017@qq.com>
2019-12-24 20:49:44 +0800

    支付页登录授权，获取token的请求参数对象

--- src/mixins/order_list/index.js
+++ src/mixins/order_list/index.js
@@ -1,6 +1,11 @@
 import wepy from 'wepy'
 
 export default class extends wepy.mixin {
+  data = {
+    // 登录请求参数对象
+    wxloginQuery: null
+  }
+
   computed = {
     // 获取收获地址对象
     address() {
@@ -27,15 +32,55 @@ export default class extends wepy.mixin {
       let total = 0
       this.chooseGoods.forEach(item => { total += item.num * item.price })
       return total * 100
+    },
+
+    // 获取token对象
+    token() {
+      return this.$parent.globalData.token
     }
   }
 
-  methods={
+  methods = {
     //  选择收获地址
     async onChooseAddress() {
       const address = await wepy.chooseAddress()
       // 更改全局地址对象
       this.$parent.setAddress(address)
+      this.$apply()
+    },
+
+    // 支付按钮
+    onSubmitOrder() {
+      console.log(77)
+    },
+
+    // 获取用户信息
+    async getUserInfo(userInfo) {
+        // 校验是否获取用户成功
+      if (!userInfo) {
+        return wepy.baseToast('授权失败')
+      }
+
+      // 获取code
+      const res = await wepy.login()
+      // 校验是否获取code成功
+      if (res.errMsg !== 'login:ok') {
+        return wepy.baseToast('授权失败')
+      }
+
+      const { encryptedData, rawData, iv, signature, code } = {...res, ...userInfo.detail}
+
+      // 处理请求参数对象
+      const wxloginQuery = {
+        encryptedData,
+        rawData,
+        iv,
+        signature,
+        code
+      }
+      // 更新用户登录请求对象
+      this.wxloginQuery = wxloginQuery
+
       this.$apply()
     }
   }
